64737ce630dc9eda4152611f7b58014c25e5c6e5
Sahrial-alam34 <sunnykcp01@gmail.com>
2023-07-10 13:25:32 +0600

    big o notation update

--- Big O Notation/timeOfOurCode.js
+++ Big O Notation/timeOfOurCode.js
@@ -6,11 +6,26 @@
 //     return total
 
 // }
+// complexity O(n)
 
-function sumOfAll(n){
-    return n * (n+1)/2
+
+
+// function sumOfAll(n){
+//     return n * (n+1)/2
+// }
+//complexity O(1)
+
+
+
+// console.log(sumOfAll(10))
+
+function multipleArray(n){
+    for (let i=0;i<=n;i++){
+        for(let j=0;j<=n ;j++){
+            console.log(i,j);
+        }
+    }
 }
 
-
-
-console.log(sumOfAll(10))
+// n+n = O(n2)
+multipleArray(5)
